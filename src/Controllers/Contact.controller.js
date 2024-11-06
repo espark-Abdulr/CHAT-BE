565631fc5be21e3650ae400fe839c54144122cd6
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2024-11-06 23:45:12 +0500

    Showing all previous contacts API DONE

--- src/Controllers/Contact.controller.js
+++ src/Controllers/Contact.controller.js
@@ -1,6 +1,8 @@
 import { UserModel } from "../Models/User.model.js";
 import { asyncHandler } from "../Utils/asyncHandler.js";
 import { ApiResponse } from "../Utils/ApiResponse.js";
+import mongoose from "mongoose";
+import MessageModel from "../Models/Chat.model.js";
 
 export const SearchContactsController = asyncHandler(async (req, res) => {
     const { searchTerm } = req.body
@@ -36,5 +38,72 @@ export const SearchContactsController = asyncHandler(async (req, res) => {
             }
         ]
     })
+    return res.status(200).json(new ApiResponse(200, { contacts }, "Users found successfully"));
+})
+
+
+export const GetAllContactsForDmController = asyncHandler(async (req, res) => {
+    let userId = req?.userId;
+    if (!userId) {
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+    const user = await UserModel.findById(userId);
+    if (!user) {
+        return res.status(404).json({ message: "User with this id not found" });
+    }
+    userId = new mongoose.Types.ObjectId(userId);
+
+    const contacts = await MessageModel.aggregate([
+        {
+            $match: {
+                $or: [
+                    { sender: userId },
+                    { recipient: userId }
+                ]
+            }
+        },
+        {
+            $sort: { createdAt: -1 }
+        },
+        {
+            $group: {
+                _id: {
+                    $cond: {
+                        if: { $eq: ["$sender", userId] },
+                        then: "$recipient",
+                        else: "$sender"
+                    }
+                },
+                lastMessageTime: { $first: "$createdAt" }
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "_id",
+                foreignField: "_id",
+                as: "contactInfo"
+            }
+        },
+        {
+            $unwind: "$contactInfo"
+        },
+        {
+            $project: {
+                _id: 1,
+                lastMessageTime: 1,
+                email: "$contactInfo.email",
+                firstName: "$contactInfo.firstName",
+                lastName: "$contactInfo.lastName",
+                profileImg: "$contactInfo.profileImg",
+                color: "$contactInfo.color",
+            }
+        },
+        {
+            $sort: { lastMessageTime: -1 }
+        }
+    ]);
+    
+    
     return res.status(200).json(new ApiResponse(200, { contacts }, "Users found successfully"));
 })
--- src/Routes/ContactRoutes..routes.js
+++ src/Routes/ContactRoutes..routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { SearchContactsController } from "../Controllers/Contact.controller.js";
+import { GetAllContactsForDmController, SearchContactsController } from "../Controllers/Contact.controller.js";
 import { verifyToken } from "../Middlewares/Auth.middleware.js";
 
 
 const contactRoutes = Router();
 
 contactRoutes.post("/search", verifyToken, SearchContactsController)
+contactRoutes.get("/get-contacts-dm", verifyToken, GetAllContactsForDmController)
 
 
 
